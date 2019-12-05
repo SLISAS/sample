49fd1ac7c6f427063fdc7efd482c9ca36df7f88d
SLISAS <alpinespringwater@vivaldi.net>
2019-12-05 11:45:15 +0900

    replace micopost_spec to user_spec to avoid failure

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -71,7 +71,7 @@ RSpec.describe User, type: :model do
 
     # ユニークなアドレス
     describe "email uniqueness" do
-      duplicate
+      # duplicate
       context "when email is dup and upcase" do
         it "should already taken (uniqueness case insensitive)" do
           user = User.create(name: "foobar", email: "foo@bar.com", password: "foobar")
@@ -177,6 +177,35 @@ RSpec.describe User, type: :model do
       it "other-users followers does not include user" do
         following.each do |u|
           expect(u.followers).not_to include(user)
+        end
+      end
+    end
+    #  マイクロポスト
+    describe "mictopost association" do
+      before { user.save }
+      # 今日の投稿・昨日の投稿
+      subject(:new_post) { FactoryBot.create(:user_post, :today, user: user) }
+      subject(:old_post) { FactoryBot.create(:user_post, :yesterday, user: user) }
+
+      # 降順に表示される
+      it "order descending" do
+        new_post
+        old_post
+        expect(user.microposts.count).to eq 2
+        expect(Micropost.all.count).to eq user.microposts.count
+        expect(user.microposts.to_a).to eq [new_post, old_post]
+      end
+
+      # ユーザー削除と同時にマイクロポストも削除
+      it "should destroy micropost depend on destroy user" do
+        new_post
+        old_post
+        my_posts = user.microposts.to_a
+        user.destroy
+        # ユーザはマイクロポストを持っている
+        expect(my_posts).not_to be_empty
+        user.microposts.each do |post|
+          expect(Micropost.where(id: post.id)).to be_empty
         end
       end
     end
