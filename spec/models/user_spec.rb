a88af5bf91201f8631ac0f5a57694eba33ebb85c
SLISAS <alpinespringwater@vivaldi.net>
2019-12-05 11:38:38 +0900

    Fix Gemfile.lock

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@
 require "spec_helper"
 require "rails_helper"
 
-属性・メソッドの検証
+# 属性・メソッドの検証
 shared_examples_for "User-model respond to attribute or method" do
   it { should respond_to(:name) }
   it { should respond_to(:email) }
@@ -26,27 +26,27 @@ RSpec.describe User, type: :model do
   it_behaves_like "User-model respond to attribute or method"
 
   describe "validations" do
-    存在性
+    # 存在性
     describe "presence" do
-      名前・メール
+      # 名前・メール
       it { should validate_presence_of :name }
       it { should validate_presence_of :email }
-      パスワード・confirmation
+      # パスワード・confirmation
       context "when pass and confirmation is not present" do
         before { user.password = user.password_confirmation = " " }
         it { should_not be_valid }
       end
     end
-    文字数
+    # 文字数
     describe "characters" do
       it { should validate_length_of(:name).is_at_most(50) }
       it { should validate_length_of(:email).is_at_most(255) }
       it { should validate_length_of(:password).is_at_least(6) }
     end
 
-    メールのフォーマット
+    # メールのフォーマット
     describe "email format" do
-      無効
+      # 無効
       context "when invalid format" do
         it "should be invalid" do
           invalid_addr = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
@@ -57,7 +57,7 @@ RSpec.describe User, type: :model do
         end
       end
 
-      有効
+      # 有効
       context "when valid format" do
         it "should be valid" do
           valid_addr = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
@@ -69,7 +69,7 @@ RSpec.describe User, type: :model do
       end
     end
 
-    ユニークなアドレス
+    # ユニークなアドレス
     describe "email uniqueness" do
       duplicate
       context "when email is dup and upcase" do
@@ -81,10 +81,10 @@ RSpec.describe User, type: :model do
         end
       end
 
-      大文字・小文字の混在
+      # 大文字・小文字の混在
       context "when mixed-case" do
         let(:mixed_case_email) { "Foo@exampLE.COM" }
-        混在していても小文字で保存される
+        # 混在していても小文字で保存される
         it "should be saved as lower-case" do
           user.email = mixed_case_email
           user.save
@@ -94,7 +94,7 @@ RSpec.describe User, type: :model do
     end
   end
 
-  パスワード認証
+  # パスワード認証
   describe "has_secure_password" do
     context "when mismatched confirmation" do
       before { user.password_confirmation = "mismatch" }
@@ -102,13 +102,13 @@ RSpec.describe User, type: :model do
     end
   end
 
-  パスワード認証
+  # パスワード認証
   describe "authenticate? method" do
     before { user.save }
     let(:found_user) { User.find_by(email: user.email) }
-    有効なパスワード
+    # 有効なパスワード
     context "when valid password" do
-      認証成功シナリオ
+      # 認証成功シナリオ
       it "success authentification" do
         should eq found_user.authenticate(user.password)
       end
@@ -116,10 +116,10 @@ RSpec.describe User, type: :model do
       it { is_expected.to be_valid }
     end
 
-    無効なパスワード
+    # 無効なパスワード
     context "when invalid password" do
       let(:incorrect) { found_user.authenticate("aaaaaaa") }
-      認証失敗シナリオ
+      # 認証失敗シナリオ
       it "fail authentication" do
         should_not eq incorrect
       end
